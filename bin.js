d734775ceeac95b47918fc34afefbb16ec5cfcf6
Maciej Krüger <mkg20001@gmail.com>
2018-08-17 12:13:01 +0200

    feat: Add WIP cli

---
+++ bin.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+'use strict'
+
+// TODO: cli to manage repos using this template
+
+/*
+
+Link on server:
+  ssl -> /etc/letsencrypt
+
+Link in repo:
+  nginx/_/*
+  nginx/conf.d/*
+  ssl/acme-wrapper.sh
+
+Cp to repo on init:
+  nginx/sites/00-default.conf
+
+*/
