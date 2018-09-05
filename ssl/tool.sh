7273a7748a998c2feb35a9a42ab943926dd79c36
Maciej Krüger <mkg20001@gmail.com>
2018-09-05 10:50:01 +0200

    Add dh params

--- ssl/tool.sh
+++ ssl/tool.sh
@@ -14,6 +14,7 @@ main() {
     renew) # renew all certs
       shift
       acme --cron "$@"
+      main dhparams
       ;;
     genconf) # generate nginx snippets for ssl
       nssl=$(readlink -f "$PWD/../nginx/_/ssl")
@@ -34,6 +35,7 @@ $hsts
 ssl_certificate /etc/ssl/letsencrypt/$d/fullchain.cer;
 ssl_certificate_key /etc/ssl/letsencrypt/$d/$d.key;
 ssl_trusted_certificate /etc/ssl/letsencrypt/$d/fullchain.cer;
+ssl_dhparam /etc/ssl/letsencrypt/$d/dhparams.pem;
 " > "$nssl/$d.conf"
         fi
       done
@@ -54,8 +56,35 @@ ssl_trusted_certificate /etc/ssl/letsencrypt/$d/fullchain.cer;
       shift
       acme "$@"
       ;;
+    dhparams)
+      now=$(date +%s)
+      for d in $(dir -w 1 acme); do
+        conf="acme/$d/$d.conf"
+        if [ -e "$conf" ]; then
+          . "$conf"
+          # 1 week = 1000*60*60*24*7 in s
+          (( last = $DH_LastRenew + 604800 ))
+          create=false
+          params="acme/$d/dhparams.pem"
+          if [ $now -gt $last ]; then
+            echo "[DH] Renewing DHParams for $d..."
+            create=true
+          elif [ ! -e "$params" ]; then
+            echo "[DH] Creating DHParams for $d..."
+            create=true
+          else
+            echo "[DH] Ignore renew DHParams (nextupdate=$(date -d@$last), domain=$d)"
+          fi
+          if $create; then
+            openssl dhparam -out "$params" -dsaparam 4096
+            cat <(cat "$conf" | grep -v DH_LastRenew) <(echo "DH_LastRenew=$now") > "$conf.new"
+            mv "$conf.new" "$conf"
+          fi
+        fi
+      done
+      ;;
     *)
-      echo "Usage: $0 {add, renew, genconf, acme}"
+      echo "Usage: $0 {add, renew, genconf, acme, dhparams}"
       ;;
   esac
 }
