a972fd7c656f476696e53a6f2e28182b3238ee40
Maciej Krüger <mkg20001@gmail.com>
2019-01-04 18:49:39 +0100

    feat: Add cloudflare only configs

---
+++ nginx/build-cf-conf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+cf_ips() {
+  echo "# https://www.cloudflare.com/ips"
+  echo
+
+  for type in v4 v6; do
+    echo "## $type"
+    curl -s "https://www.cloudflare.com/ips-$type" | sed "s|^|allow |g" | sed "s|\$|;|g"
+    echo
+  done
+
+  echo "# Generated at $(LC_ALL=C date)"
+}
+
+cf_ips > _/allow-cf.conf
+echo "Generated _/allow-cf.conf!"
